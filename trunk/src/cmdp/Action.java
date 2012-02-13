7a86bfa33d70dc012608bc3633e47f83cb174246
zahra.zamani@gmail.com <zahra.zamani@gmail.com@e88f101c-7640-4863-68ea-b3a329cb5f23>
2012-02-13 08:15:35 +0000

    Changed for added observation model (POMDP)

--- trunk/src/cmdp/Action.java
+++ trunk/src/cmdp/Action.java
@@ -34,8 +34,10 @@ public class Action {
 
 	/* Local vars */
 	public CMDP _mdp; // MDP of which this action is a part
+	public CPOMDP _pomdp;
 	public String _sName; // Name of this action
 	public HashMap<String, Integer> _hmVar2DD;
+	public HashMap<String, Integer> _hmObs2DD;
 	public Integer _reward;
 
 	/**
@@ -50,6 +52,16 @@ public class Action {
 		buildAction(cpt_desc);
 	}
 
+	public Action(CPOMDP pomdp, String name, HashMap<String, ArrayList> cpt_desc,HashMap<String, ArrayList> obs_desc, Integer reward) {
+
+		_pomdp = pomdp;
+		_sName = name;
+		_hmVar2DD = new HashMap<String, Integer>();
+		_hmObs2DD = new HashMap<String, Integer>();
+		_reward = reward;
+		buildAction(cpt_desc,obs_desc);
+	}
+
 	/**
 	 * Build action description DDs
 	 **/
@@ -57,10 +69,10 @@ public class Action {
 
 		// Head will be for current next-state
 		for (Map.Entry<String,ArrayList> e : cpt_desc.entrySet()) {
-			
+
 			String var = e.getKey();
 			int xadd = _mdp._context.buildCanonicalXADD(e.getValue());
-					
+
 			// If a boolean variable, need to construct dual action diagram
 			//System.out.println(var + " vs " + _mdp._alBVars);
 			if (_mdp._alBVars.contains(var.substring(0, var.length() - 1))) {
@@ -73,25 +85,97 @@ public class Action {
 				_hmVar2DD.put(var, xadd);
 			} else{
 				_hmVar2DD.put(var, xadd);
-				}
+			}
 		}
 	}
 
-	public String toString() {
-		StringBuffer sb = new StringBuffer();
-		sb.append(_sName + ":\n");
-		for (Map.Entry<String,Integer> me : _hmVar2DD.entrySet()) {
-			XADD.XADDNode n = _mdp._context.getNode(me.getValue());
-			sb.append("*****\n" + me.getKey() + " " + n.collectVars() + ":\n" + 
-					  _mdp._context.getString(me.getValue()) + "\n");
-		}
-		
-		XADD.XADDNode r = _mdp._context.getNode(_reward);
-		sb.append("*****\nReward: " + r.collectVars() + ":\n" + 
-				  _mdp._context.getString(_reward) + "\n");
-		
-		sb.append("*****\n");
+	public void buildAction(HashMap<String,ArrayList> cpt_desc,HashMap<String,ArrayList> obs_desc) {
 
-		return sb.toString();
+		// Head will be for current next-state
+		for (Map.Entry<String,ArrayList> e : cpt_desc.entrySet()) {
+
+			String var = e.getKey();
+			int xadd = _pomdp._context.buildCanonicalXADD(e.getValue());
+
+			// If a boolean variable, need to construct dual action diagram
+			//System.out.println(var + " vs " + _mdp._alBVars);
+			if (_pomdp._alBVars.contains(var.substring(0, var.length() - 1))) {
+				int var_index = _pomdp._context.getVarIndex( _pomdp._context.new BoolDec(var/* + "'"*/), false);
+				int high_branch = xadd;
+				int low_branch = _mdp._context.apply(
+						_pomdp._context.getTermNode(XADD.ONE), high_branch, XADD.MINUS);
+				xadd = _pomdp._context.getINode(var_index, low_branch, high_branch);
+				xadd = _pomdp._context.makeCanonical(xadd);
+				_hmVar2DD.put(var, xadd);
+			} else{
+				_hmVar2DD.put(var, xadd);
+			}
+		}
+
+		for (Map.Entry<String,ArrayList> e : obs_desc.entrySet()) {
+
+			String var = e.getKey();
+			int xadd = _pomdp._context.buildCanonicalXADD(e.getValue());
+
+			// If a boolean variable, need to construct dual action diagram
+			//System.out.println(var + " vs " + _mdp._alBVars);
+			if (_pomdp._alBVars.contains(var.substring(0, var.length() - 1))) {
+				int var_index = _pomdp._context.getVarIndex( _pomdp._context.new BoolDec(var/* + "'"*/), false);
+				int high_branch = xadd;
+				int low_branch = _mdp._context.apply(
+						_pomdp._context.getTermNode(XADD.ONE), high_branch, XADD.MINUS);
+				xadd = _pomdp._context.getINode(var_index, low_branch, high_branch);
+				xadd = _pomdp._context.makeCanonical(xadd);
+				_hmObs2DD.put(var, xadd);
+			} else{
+				_hmObs2DD.put(var, xadd);
+			}
+		}
+	}
+
+
+	public String toString() 
+	{
+		if (_hmObs2DD.size()>0)
+		{
+			StringBuffer sb = new StringBuffer();
+			sb.append(_sName + ":\n");
+			for (Map.Entry<String,Integer> me : _hmVar2DD.entrySet()) {
+				XADD.XADDNode n = _pomdp._context.getNode(me.getValue());
+				sb.append("*****\n" + me.getKey() + " " + n.collectVars() + ":\n" + 
+						_pomdp._context.getString(me.getValue()) + "\n");
+			}
+			for (Map.Entry<String,Integer> me : _hmObs2DD.entrySet()) 
+			{
+				XADD.XADDNode n = _pomdp._context.getNode(me.getValue());
+				sb.append("*****\n" + me.getKey() + " " + n.collectVars() + ":\n" + 
+						_pomdp._context.getString(me.getValue()) + "\n");
+			}
+			XADD.XADDNode r = _pomdp._context.getNode(_reward);
+			sb.append("*****\nReward: " + r.collectVars() + ":\n" + 
+					_pomdp._context.getString(_reward) + "\n");
+
+			sb.append("*****\n");
+
+			return sb.toString();
+		}
+		else 
+		{
+			StringBuffer sb = new StringBuffer();
+			sb.append(_sName + ":\n");
+			for (Map.Entry<String,Integer> me : _hmVar2DD.entrySet()) {
+				XADD.XADDNode n = _mdp._context.getNode(me.getValue());
+				sb.append("*****\n" + me.getKey() + " " + n.collectVars() + ":\n" + 
+						_mdp._context.getString(me.getValue()) + "\n");
+			}
+
+			XADD.XADDNode r = _mdp._context.getNode(_reward);
+			sb.append("*****\nReward: " + r.collectVars() + ":\n" + 
+					_mdp._context.getString(_reward) + "\n");
+
+			sb.append("*****\n");
+
+			return sb.toString();
+		}
 	}
 }
