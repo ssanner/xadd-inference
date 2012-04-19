92e6aac4ff1c78f58100271759fead1f1d703a02
ssanner <ssanner@e88f101c-7640-4863-68ea-b3a329cb5f23>
2012-04-19 04:14:10 +0000

    Reverted some unneeded updates in r230

--- src/sve/GraphicalModel.java
+++ src/sve/GraphicalModel.java
@@ -16,15 +16,14 @@ import xadd.XADD.ArithExpr;
 import xadd.XADD.BoolDec;
 import xadd.XADD.DoubleExpr;
 import xadd.XADD.VarExpr;
-import xadd.XADDParseEvent;
 
-public class GraphicalModel implements Observer {
+public class GraphicalModel {
 
 	public ArrayList<String> _alBVarsTemplate = null;
 	public ArrayList<String> _alCVarsTemplate = null;
-	public HashMap<String, ArrayList> _hmVar2cptTemplate = null;
-	public HashMap<String, Double> _hmCVar2MinValue = null;
-	public HashMap<String, Double> _hmCVar2MaxValue = null;
+	public HashMap<String,ArrayList> _hmVar2cptTemplate = null;
+	public HashMap<String,Double> _hmCVar2MinValue = null;
+	public HashMap<String,Double> _hmCVar2MaxValue = null;
 	public ArrayList<Factor> _alFactors = null;
 	public HashSet<String> _hsVariables = null;
 	public XADD _context = null;
@@ -32,62 +31,55 @@ public class GraphicalModel implements Observer {
 	public class Factor {
 		public int _xadd;
 		public HashSet<String> _vars;
-		public XADD _localContext; // Outside classes cannot see _context, so
-									// store locally
-
+		public XADD _localContext; // Outside classes cannot see _context, so store locally
 		public Factor(int xadd) {
 			_xadd = xadd;
 			_localContext = _context;
 			_vars = _context.collectVars(_xadd);
 			_hsVariables.addAll(_vars);
 		}
-
 		public String toString() {
 			return _vars + ":\n" + _context.getString(_xadd);
 		}
 	}
-
+	
 	public GraphicalModel(String filename) {
-		_hmVar2cptTemplate = new HashMap<String, ArrayList>();
+		_hmVar2cptTemplate = new HashMap<String,ArrayList>();
 		_alBVarsTemplate = new ArrayList<String>();
 		_alCVarsTemplate = new ArrayList<String>();
-		_hmCVar2MinValue = new HashMap<String, Double>();
-		_hmCVar2MaxValue = new HashMap<String, Double>();
-		XADD.addParseEvent(this);
+		_hmCVar2MinValue = new HashMap<String,Double>();
+		_hmCVar2MaxValue = new HashMap<String,Double>();
 		ArrayList l = HierarchicalParser.ParseFile(filename);
-		// System.out.println(l);
-		// XADDParseEvent pe = new XADDParseEvent(this);
+		//System.out.println(l);
 		parseGMTemplate(l);
-		
 	}
 
-	public void instantiateGMTemplate(HashMap<String, ArrayList<Integer>> var2expansion) {
+	public void instantiateGMTemplate(HashMap<String,ArrayList<Integer>> var2expansion) {
 
 		_context = new XADD();
-		_alFactors = new ArrayList<Factor>();
+		_alFactors = new ArrayList<Factor>(); 
 		_hsVariables = new HashSet<String>();
-
+		
 		// Assumptions: * at most one free index per variable
-		// * if a variable x_i uses x_i' then it will be defined by x_i'
-		// with a separate x_1... both x_i' and x_1 (or lowest index) must be
-		// present
-		// * if an index appears in a cpf, it will appear in the head variable
-		// * indices must start with letter, e.g., x_1 will be interpreted as is
-
+		//              * if a variable x_i uses x_i' then it will be defined by x_i'
+		//                with a separate x_1... both x_i' and x_1 (or lowest index) must be present
+		//              * if an index appears in a cpf, it will appear in the head variable
+		//              * indices must start with letter, e.g., x_1 will be interpreted as is
+		
 		// Expand variables and ensure properly defined in XADD
 		ArrayList[] both_lists = new ArrayList[] { _alBVarsTemplate, _alCVarsTemplate };
 		for (ArrayList list : both_lists) {
-			for (String var : (ArrayList<String>) list) {
-
+			for (String var : (ArrayList<String>)list) {
+					
 				String split[] = var.split("_");
 				if (split.length == 1 || (split.length == 2 && Character.isDigit(split[1].charAt(0)))) {
-
+					
 					addXADDVar(var, list == _alBVarsTemplate);
-					int cpt_id = getXADD(var, list == _alBVarsTemplate);
+					int cpt_id = getXADD(var, list == _alBVarsTemplate);					
 					_alFactors.add(new Factor(cpt_id));
-
+					
 				} else if (split.length == 2) {
-
+					
 					String var_part = split[0];
 					String index = split[1];
 					ArrayList<Integer> index_values = var2expansion.get(index);
@@ -102,33 +94,30 @@ public class GraphicalModel implements Observer {
 					}
 					addXADDVar(prime ? var + "'" : var, list == _alBVarsTemplate);
 					int cpt_template = getXADD(prime ? var + "'" : var, list == _alBVarsTemplate);
-					// System.out.println("Template: " + var + "\n" +
-					// _context.getString(cpt_template));
-
+					//System.out.println("Template: " + var + "\n" + _context.getString(cpt_template));
+					
 					// Process each index var
-					for (int i = 0; i < index_values.size(); i++) {
+					for (int i = 0; i < index_values.size(); i++) { 
 						Integer index_value = index_values.get(i);
-
+						
 						addXADDVar(var_part + "_" + index_value, list == _alBVarsTemplate);
-
+						
 						if (prime && i == 0) {
-							// If using prime, get first index CPF directly from
-							// cpt lookup
+							// If using prime, get first index CPF directly from cpt lookup 
 							// table (must be defined)
 							int cpt_id = getXADD(var_part + "_" + index_value, list == _alBVarsTemplate);
 							_alFactors.add(new Factor(cpt_id));
 						} else { // i > 0
-
+							
 							if (prime)
 								addXADDVar(var_part + "_" + (index_value - 1), list == _alBVarsTemplate);
 
-							// Need to process all other variables in XADD and
-							// substitute
+							// Need to process all other variables in XADD and substitute 
 							// into XADD template as needed
 							HashMap<String, ArithExpr> subst = new HashMap<String, ArithExpr>();
 							HashSet<String> all_vars = _context.collectVars(cpt_template);
 							for (String xadd_var : all_vars) {
-
+								
 								if (xadd_var.endsWith("'"))
 									xadd_var = xadd_var.substring(0, xadd_var.length() - 1);
 								String split2[] = xadd_var.split("_");
@@ -136,23 +125,23 @@ public class GraphicalModel implements Observer {
 									if (prime) {
 										subst.put(xadd_var, new VarExpr(split2[0] + "_" + (index_value - 1)));
 										subst.put(xadd_var + "'", new VarExpr(split2[0] + "_" + index_value));
-									} else
+									} else 
 										subst.put(xadd_var, new VarExpr(split2[0] + "_" + index_value));
 								}
 							}
-
+														
 							int cpt_id = _context.substitute(cpt_template, subst);
 							_alFactors.add(new Factor(cpt_id));
 						}
 					}
-
+					
 				} else {
 					exit("Var '" + var + "' can only have one index, it has " + split.length);
 				}
 			}
 		}
 	}
-
+	
 	protected int getXADD(String var, boolean is_bool) {
 		ArrayList cpt_desc = _hmVar2cptTemplate.get(var);
 		if (cpt_desc == null)
@@ -160,11 +149,12 @@ public class GraphicalModel implements Observer {
 		int cpt_id = _context.buildCanonicalXADD(cpt_desc);
 		if (is_bool) {
 			// Fill in false side of diagram if boolean
-			int var_index = _context.getVarIndex(_context.new BoolDec(var/* + "'" */), false);
+			int var_index = _context.getVarIndex(_context.new BoolDec(var/* + "'"*/), false);
 			if (var_index == 0)
 				exit("Could not get XADD variable index for " + var);
 			int high_branch = cpt_id;
-			int low_branch = _context.apply(_context.getTermNode(XADD.ONE), high_branch, XADD.MINUS);
+			int low_branch = _context.apply(
+					_context.getTermNode(XADD.ONE), high_branch, XADD.MINUS);
 			cpt_id = _context.getINode(var_index, low_branch, high_branch);
 			cpt_id = _context.makeCanonical(cpt_id);
 		}
@@ -172,7 +162,7 @@ public class GraphicalModel implements Observer {
 	}
 
 	protected void addXADDVar(String var, boolean is_bool) {
-
+		
 		if (is_bool)
 			_context.getVarIndex(_context.new BoolDec(var), true);
 		else {
@@ -193,7 +183,7 @@ public class GraphicalModel implements Observer {
 	}
 
 	public void parseGMTemplate(ArrayList l) {
-
+		
 		// Set up variables
 		Iterator i = l.iterator();
 		Object o = i.next();
@@ -201,7 +191,7 @@ public class GraphicalModel implements Observer {
 			exit("Missing cvariable declarations: " + o);
 		}
 		o = i.next();
-		_alCVarsTemplate = (ArrayList<String>) ((ArrayList) o).clone();
+		_alCVarsTemplate = (ArrayList<String>) ((ArrayList) o).clone();		
 		o = i.next();
 		if (!(o instanceof String) || !((String) o).equalsIgnoreCase("min-values")) {
 			exit("Missing min-values declarations: " + o);
@@ -211,22 +201,21 @@ public class GraphicalModel implements Observer {
 			if (index >= ((ArrayList) o).size())
 				exit("Min-values size does not match var list size");
 			String var = _alCVarsTemplate.get(index);
-
+			
 			String val = ((ArrayList) o).get(index).toString();
-			if (!val.trim().equalsIgnoreCase("x"))
-				try {
-					double min_val = Double.parseDouble(val);
-					_hmCVar2MinValue.put(var, min_val);
-
-					// Add root mapping to: x_i -> val then x -> val as well
-					String split[] = var.split("_");
-					String var_root = (split.length == 2) ? split[0] : null;
-					if (var_root != null)
-						_hmCVar2MinValue.put(var_root, min_val);
-				} catch (NumberFormatException nfe) {
-					System.out.println("\nIllegal min-value: " + var + " = " + val + " @ index " + index);
-					System.exit(1);
-				}
+			if (!val.trim().equalsIgnoreCase("x")) try {
+				double min_val = Double.parseDouble(val);
+				_hmCVar2MinValue.put(var, min_val);
+				
+				// Add root mapping to: x_i -> val then x -> val as well
+				String split[] = var.split("_");
+				String var_root = (split.length == 2) ? split[0] : null;
+				if (var_root != null)
+					_hmCVar2MinValue.put(var_root, min_val);
+			} catch (NumberFormatException nfe) {
+				System.out.println("\nIllegal min-value: " + var + " = " + val + " @ index " + index);
+				System.exit(1);
+			}
 		}
 		o = i.next();
 		if (!(o instanceof String) || !((String) o).equalsIgnoreCase("max-values")) {
@@ -238,20 +227,19 @@ public class GraphicalModel implements Observer {
 				exit("Max-values size does not match var list size");
 			String var = _alCVarsTemplate.get(index);
 			String val = ((ArrayList) o).get(index).toString();
-			if (!val.trim().equalsIgnoreCase("x"))
-				try {
-					double max_val = Double.parseDouble(val);
-					_hmCVar2MaxValue.put(var, max_val);
-
-					// Add root mapping to: x_i -> val then x -> val as well
-					String split[] = var.split("_");
-					String var_root = (split.length == 2) ? split[0] : null;
-					if (var_root != null)
-						_hmCVar2MaxValue.put(var_root, max_val);
-				} catch (NumberFormatException nfe) {
-					System.out.println("\nIllegal max-value: " + var + " = " + val + " @ index " + index);
-					System.exit(1);
-				}
+			if (!val.trim().equalsIgnoreCase("x")) try {
+				double max_val = Double.parseDouble(val);
+				_hmCVar2MaxValue.put(var, max_val);
+				
+				// Add root mapping to: x_i -> val then x -> val as well
+				String split[] = var.split("_");
+				String var_root = (split.length == 2) ? split[0] : null;
+				if (var_root != null)
+					_hmCVar2MaxValue.put(var_root, max_val);
+			} catch (NumberFormatException nfe) {
+				System.out.println("\nIllegal max-value: " + var + " = " + val + " @ index " + index);
+				System.exit(1);
+			}
 		}
 		o = i.next();
 		if (!(o instanceof String) || !((String) o).equalsIgnoreCase("bvariables")) {
@@ -269,10 +257,10 @@ public class GraphicalModel implements Observer {
 			if (!(cpt instanceof ArrayList)) {
 				exit("Decision diagram (...) must follow cpt: " + cpt);
 			}
-			_hmVar2cptTemplate.put((String) o, (ArrayList) cpt);
+			_hmVar2cptTemplate.put((String)o, (ArrayList)cpt);
 		}
 	}
-
+	
 	public String toString() {
 		StringBuilder sb = new StringBuilder();
 		sb.append("Graphical Model Template:\n================\n");
@@ -289,12 +277,12 @@ public class GraphicalModel implements Observer {
 			sb.append("All variables in XADD: " + _context._alOrder + "\n");
 			for (Factor f : _alFactors) {
 				sb.append(" - " + f + "\n");
-				// _context.getGraph(f._xadd).launchViewer();
+				//_context.getGraph(f._xadd).launchViewer();
 			}
 		}
 		return sb.toString();
 	}
-
+	
 	public void exit(String msg) {
 		System.err.println(msg);
 		new Exception().printStackTrace(System.err);
@@ -305,20 +293,14 @@ public class GraphicalModel implements Observer {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// GraphicalModel gm = new GraphicalModel("./src/sve/test2.gm");
+		//GraphicalModel gm = new GraphicalModel("./src/sve/test2.gm");
 		GraphicalModel gm = new GraphicalModel("./src/sve/tracking.gm");
-		// Query q = new Query("./src/sve/test.query");
+		//Query q = new Query("./src/sve/test.query");
 		Query q = new Query("./src/sve/tracking.query.6");
-		// System.out.println(gm);
+		//System.out.println(gm);
 		System.out.println(q);
 		gm.instantiateGMTemplate(q._hmVar2Expansion);
-		
-	//	System.out.println(gm);
-	}
-
-	@Override
-	public void update(Observable o, Object arg) {
-		System.out.println(">>>>>" + arg);
+		System.out.println(gm);
 	}
 
 }
