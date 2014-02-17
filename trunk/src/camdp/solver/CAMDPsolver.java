24c50a3d07967ed4d86a0464920aa4895c973e90
ludy17ludy@gmail.com <ludy17ludy@gmail.com@e88f101c-7640-4863-68ea-b3a329cb5f23>
2014-02-17 23:47:30 +0000

    Improvements to solverMain, CRTDP and VI, plus creation of CRTDPFH

--- trunk/src/camdp/solver/CAMDPsolver.java
+++ trunk/src/camdp/solver/CAMDPsolver.java
@@ -1,5 +1,6 @@
 package camdp.solver;
 
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.PrintStream;
@@ -16,71 +17,97 @@ public abstract class CAMDPsolver {
 	public XADD _context;
 	public Integer _valueDD;
 	public String _solveMethod = null;
-	
-	
-	public final static boolean DISPLAY_PREMAX_Q = false;
-	public final static boolean DISPLAY_POSTMAX_Q = false;
-	public final static boolean DISPLAY_V = true;
-	public final static boolean DISPLAY_MAX = false;
-	
-	/* Cache maintenance */
-	public final static boolean ALWAYS_FLUSH = false; // Always flush DD caches?
-	public final static double FLUSH_PERCENT_MINIMUM = 0.3d; // Won't flush until < amt
-	
+		
 	/* Approximation Parameters */
 	public double APPROX_ERROR = 0.0d;
 	public boolean APPROX_ALWAYS = false;
 	public boolean APPROX_PRUNING = false;
 	public boolean COMPARE_OPTIMAL = false;
 	
+	/* For printing */
+	public final static String RESULTS_DIR = "./results"; // Diagnostic output destination
+	public static DecimalFormat _df = new DecimalFormat("#.########");
+	public PrintStream _logStream = null;
+	public PrintStream _resultStream = null;	
+	public final static String ASCII_BAR = "%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%"; // Display shortcut
+	
+	/* Time & Memory Management */
+	public long _lTime = 0;
+	
+	/*Solution maintenance */
+	public ArrayList<Integer> solutionDDList = new ArrayList<Integer>();
+	public ArrayList<Long> solutionTimeList = new ArrayList<Long>();
+	public ArrayList<Integer> solutionNodeList = new ArrayList<Integer>();
+	public ArrayList<Double> solutionInitialSValueList = new ArrayList<Double>();
+	public ArrayList<Double> solutionMaxValueList = new ArrayList<Double>();
+	
+	/* *********************** Methods *********************** */
+	public void setupSolution(){
+		solutionDDList.add(null);
+		solutionTimeList.add(null);
+		solutionNodeList.add(null);
+		solutionInitialSValueList.add(null);
+		solutionMaxValueList.add(null);
+	}
+
 	public void setApproxTest(double eps, PrintStream log, boolean always) {
 		APPROX_ERROR = eps;
 		_resultStream = log;
 		APPROX_ALWAYS = always;
 		COMPARE_OPTIMAL = true;
 	}
-
-	
-	/* For printing */
-	public final static String RESULTS_DIR = "./results"; // Diagnostic output destination
-	
-	public static DecimalFormat _df = new DecimalFormat("#.########");
-	public PrintStream _logStream = null;
-	public PrintStream _resultStream = null;	
-	public final static String ASCII_BAR = "%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%"; // Display shortcut
-	
-	/* *********************** Methods *********************** */
 	
 	public int solve(int max_iter){
 		System.out.println("Abstract CAMDP solver.");
 		return 0;
 	}
-	
-	public void flushCaches() { }
-    public void flushCaches(List<Integer> special_nodes) {}
-    
+	    
     public void makeResultStream(){
 		int filenamestart = _mdp._problemFile.lastIndexOf('/');
 		String filename = _mdp._problemFile.substring(filenamestart,_mdp._problemFile.length()-5);
-		String problemType = _mdp.CONTINUOUS_ACTIONS? "/contact/":"/discact"; 
-		System.out.println("testing filename:" + problemType + filename);
+		String problemType = _mdp.CONTINUOUS_ACTIONS? "/contact":"/discact"; 
+		String dir = RESULTS_DIR + problemType + filename;
+		
+		//System.out.println("testing filename:" + dir + "/" + _solveMethod + ".rslt");
     	try{
-    		_resultStream = new PrintStream(new FileOutputStream(RESULTS_DIR + problemType + filename + _solveMethod + ".rslt"));
+    		new File(dir).mkdirs();
+    		_resultStream = new PrintStream(new FileOutputStream(dir + "/" + _solveMethod + ".rslt"));
     	}
     	catch (FileNotFoundException e){
-    		System.err.println("Couldn't create result Stream for: "+RESULTS_DIR + problemType + filename + _solveMethod + ".rslt\nException:"+e);
+    		System.err.println("Couldn't create result Stream for: "+dir + "/" + _solveMethod + ".rslt\nException:"+e);
     	}
     }
     
-	public String makeXADDLabel(String xadd, int iter, double approx)
-	{
-		return  xadd+"^"+iter+"-"+String.format("%03d",Math.round(1000*approx));
+    public void printResults(){
+    }
+    
+    public void saveResults(){
+    	//Results: N (trial or Iter), Time, Nodes, InitialS Value.
+    	for(int i=1; i< solutionDDList.size(); i++){
+    		_resultStream.format("%d %d %d %f\n", i, solutionTimeList.get(i), solutionNodeList.get(i), (_mdp._initialS != null) ? solutionInitialSValueList.get(i): "0");
+    	}
+    }
+    
+    /////// Time Management utilities ////////////////////// 
+
+	// Reset elapsed time
+	public void resetTimer() {
+		_lTime = System.currentTimeMillis(); 
 	}
 
-    public String makeXADDLabel(String xadd, int trial, int depth, double approx)
-	{
-		return  xadd+" T"+trial+"d"+depth+" -a"+String.format("%03d",Math.round(1000*approx));
+	// Get the elapsed time since resetting the timer
+	public long getElapsedTime() {
+		return System.currentTimeMillis() - _lTime;
 	}
 
-
+	public void flushCaches(){
+		flushCaches(new ArrayList<Integer>());
+	}
+	public void flushCaches(List<Integer> specialNodes){
+		ArrayList<Integer> moreSpecialNodes = new ArrayList<Integer>();
+		moreSpecialNodes.addAll(specialNodes);
+		moreSpecialNodes.add(_valueDD);
+		for(int i=1;i<solutionDDList.size();i++) moreSpecialNodes.add(solutionDDList.get(i));
+		_mdp.flushCaches(moreSpecialNodes);
+	}
 }
--- trunk/src/camdp/solver/Main.java
+++ trunk/src/camdp/solver/Main.java
@@ -13,77 +13,122 @@ import camdp.CAMDP;
  **/
 
 public class Main {
-	
+    
+	//Running Configurations
 	private static int VERBOSE = 1;
-	private static int SOLUTION = 2; // 1 -> Value Iteration 2-> RTDP
-	private static int NTRIALS = 30; // For RTDP solution
+    private static int SOLUTION = 3; // 1 -> Value Iteration 2-> RTDP 3-> Vi first, then RTDP 4-> RTDPFH
+    private static int NTRIALS = 10; // For RTDP and RTDPFH solution
+    
+    // Results Configurations
+    private static final boolean SAVE_RESULTS = true;
+	private static final boolean PRINT_RESULTS = true;
+	private static final boolean COMPARE_RESULTS = false;
 	
-	public static void Usage() {
-		System.out.println("\nUsage: MDP-filename #iter display-2D? display-3D? [dApproxPrune]");
-		System.exit(1);
-	}
+    
+    public static void Usage() {
+        System.out.println("\nUsage: MDP-filename #iter display-2D? display-3D? [dApproxPrune]");
+        System.exit(1);
+    }
 
-	public static void main(String args[]) {
-		if (args.length < 4 || args.length >5) {
-			Usage();
-		}
-		
-		// Parse problem filename
-		String filename = args[0];
+    public static void main(String args[]) {
+        if (args.length < 4 || args.length >5) {
+            Usage();
+        }
+        
+        // Parse problem filename
+        String filename = args[0];
 
-		// Parse iterations
-		int iter = -1;
-		try {
-			iter = Integer.parseInt(args[1]);
-		} catch (NumberFormatException nfe) {
-			System.out.println("\nIllegal iteration value\n");
-			Usage();
-		}
+        // Parse iterations
+        int iter = -1;
+        try {
+            iter = Integer.parseInt(args[1]);
+        } catch (NumberFormatException nfe) {
+            System.err.println("\nIllegal iteration value\n");
+            Usage();
+        }
 
-		// Build a CAMDP, display, solve
-		CAMDP mdp = new CAMDP(filename);
-		mdp.DISPLAY_2D = Boolean.parseBoolean(args[2]);
-		mdp.DISPLAY_3D = Boolean.parseBoolean(args[3]);
-		
-		//optional argument modifies 
-		if (args.length == 5){
-			mdp.APPROX_ERROR = Double.parseDouble(args[4]);
-		}
-		
-		System.out.println("Main Solution Start:");
-		if (VERBOSE > 1){
-			System.out.println(mdp.toString(false, false));
-		}
-		
-		CAMDPsolver solver = null;
-		switch (SOLUTION){
-		case 1:
-			 solver = new VI(mdp);
-			 break;
-		case 2:
-			if (!mdp._hsBoolIVars.isEmpty() || !mdp._hsContIVars.isEmpty() || !mdp._hsNoiseVars.isEmpty()){
-				System.err.println("CAMDP Main Fail: RTDP Solution incompatible with intermediate and noise Vars!");
-				System.exit(1);
-			}
-			solver = new CRTDP(mdp,NTRIALS);
-			break;
-		default:
-			System.out.println("\n Invalid Solution Method!");
-			System.exit(1);
-		}
-		int used = solver.solve(iter);
-		
-		if (VERBOSE > 0){
-			System.out.println("\nSolution complete, required " + 
-					used + " / " + iter + " iterations or trials.");
-			if (VERBOSE > 1){
-				System.out.println("Cache Sizes before Flush: ");
-				mdp._context.showCacheSize();
-				mdp.flushCaches(true);
-				System.out.println("Cache Sizes after Flush:");
-				mdp._context.showCacheSize();
-			}
-		}
-		System.out.println("MAIN-FINISH");
-	}
+        // Build a CAMDP, display, solve
+        CAMDP mdp = new CAMDP(filename);
+        mdp.DISPLAY_2D = Boolean.parseBoolean(args[2]);
+        mdp.DISPLAY_3D = Boolean.parseBoolean(args[3]);
+        
+        //optional argument modifies 
+        if (args.length == 5){
+            mdp.APPROX_ERROR = Double.parseDouble(args[4]);
+        }
+        
+        if (VERBOSE >0) System.out.println("Main Solution Start:");
+        if (VERBOSE > 1){
+            System.out.println(mdp.toString(false, false));
+        }
+        
+        switch (SOLUTION){
+        case 1:
+            CAMDPsolver solver = new VI(mdp);
+            int used = solver.solve(iter);
+            if (SAVE_RESULTS) solver.saveResults();
+            if (PRINT_RESULTS) solver.printResults();
+
+            if (VERBOSE > 0) System.out.println("Value Iteration Solution complete, required " + used + " / " + iter + " iterations.");
+            break;
+             
+        case 2:
+            checkInitialS(mdp);
+            solver = new CRTDP(mdp,NTRIALS);
+            used = solver.solve(iter);
+            if (SAVE_RESULTS) solver.saveResults();
+            if (PRINT_RESULTS) solver.printResults();
+
+
+            if (VERBOSE > 0) System.out.println("Continuous RTDP Solution complete, " + used + " trials of depth " + iter + ".");
+            break;
+        case 3:
+            checkInitialS(mdp);
+            CAMDPsolver solver1 = new VI(mdp);
+            int used1 = solver1.solve(iter);
+            CAMDPsolver solver2 = new CRTDP(mdp, NTRIALS);
+            int used2 = solver2.solve(iter);
+            if (SAVE_RESULTS){
+            	solver1.saveResults();
+            	solver2.saveResults();
+            }
+            if (PRINT_RESULTS){
+            	solver1.printResults();
+            	solver2.printResults();
+            }
+            if (COMPARE_RESULTS){}
+            if (VERBOSE > 0) {
+            	System.out.println("Compare Solution complete, VI required " + used1 + " / " + iter + " iterations.");
+            	System.out.println("Continuous RTDP required " + used2 + " trials of depth " + iter + ".");
+            }
+            break;
+        case 4:
+            checkInitialS(mdp);
+            solver = new CRTDPFH(mdp,NTRIALS, iter);
+            used = solver.solve(iter);
+            if (SAVE_RESULTS) solver.saveResults();
+            if (PRINT_RESULTS) solver.printResults();
+
+
+            if (VERBOSE > 0) System.out.println("Continuous RTDPFH Solution complete, " + used + " trials of depth " + iter + ".");
+            break;            
+            
+    default:
+            System.err.println("\n Invalid Solution Method!");
+            System.exit(1);
+        }
+        
+        if (VERBOSE >0) System.out.println("MAIN-FINISH");
+    }
+
+    private static void checkInitialS(CAMDP mdp) {
+        if (!mdp._hsBoolIVars.isEmpty() || !mdp._hsContIVars.isEmpty() || !mdp._hsNoiseVars.isEmpty()){
+            System.err.println("CAMDP Solver Main Fail: RTDP Solution incompatible with intermediate and noise Vars!");
+            System.exit(1);
+        }
+        if (mdp._initialS == null){
+        	System.err.println("CAMDP Solver Main Fail: RTDP Solution incompatible with undefined initial state.");
+            System.exit(1);
+        }
+    }
 }
