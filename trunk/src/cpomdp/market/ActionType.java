cea5b7b145a43a0f4756bb1e544b57337b2b00b2
shamin.kinathil@gmail.com <shamin.kinathil@gmail.com@e88f101c-7640-4863-68ea-b3a329cb5f23>
2014-09-23 07:44:36 +0000

    Initial commit of cpomdp.market package

---
+++ trunk/src/cpomdp/market/ActionType.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ */
+package cpomdp.market;
+
+/**
+ * Action set specification. Can be either Discrete (finite set) or Continuous 
+ * (uncountable set)  
+ * 
+ * @author skinathil
+ *
+ */
+public enum ActionType {
+	DISCRETE,
+	CONTINUOUS
+}
