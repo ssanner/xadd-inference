b530a697d83c33a88dde7e4479402fce24de6bda
shamin.kinathil <shamin.kinathil@gmail.com>
2014-09-26 07:16:48 +0000

    Add updated PBVIDiscreteAction.java, GammaSet.java class and AlphaVector.java. Add new uncertainty_belief_point.xadd

--- src/cpomdp/market/AlphaVector.java
+++ src/cpomdp/market/AlphaVector.java
@@ -17,30 +17,64 @@ import cpomdp.market.utils.XADDHelper;
  */
 public class AlphaVector {
 	
-	private Map.Entry<String, HashMap<String, ArithExpr>> action;
-	private Map.Entry<String, Integer> observation;
-	private String observationName;
+	private Map.Entry<String, HashMap<String, ArithExpr>> action = null;
+	private Map.Entry<String, Integer> observation = null;
+	private String actionName = null;
+	private String observationName = null;
 	private Integer horizon = null;
-	private Integer valueXADD = null;
+	private Integer valueXADD = XADDHelper.ZeroXADD();
 	
-	public AlphaVector(Integer horizonNum, 
-								Map.Entry<String, HashMap<String, ArithExpr>> actionEntry, 
-								String obsName) {
+//	public AlphaVector(Integer horizonNum, 
+//			Map.Entry<String, HashMap<String, ArithExpr>> actionEntry, String obsName) {
+//		
+//		this.setHorizon(horizonNum);
+//		this.setAction(actionEntry);
+//		this.setObservation(obsName);
+//				
+//		// Set the default value to be Zero
+////		this.setValueXADD(XADDHelper.ZeroXADD());
+//	}
+
+	public AlphaVector(Integer horizonNum, String actionName, String obsName) {
 		
 		this.setHorizon(horizonNum);
-		this.setAction(actionEntry);
+		this.setActionName(actionName);
 		this.setObservation(obsName);
 				
 		// Set the default value to be Zero
-		this.setValueXADD(XADDHelper.ZeroXADD());
+//		this.setValueXADD(XADDHelper.ZeroXADD());
+	}
+	
+	private void setActionName(String actionName2) {
+		// TODO Auto-generated method stub
+		this.actionName = actionName2;
 	}
 
+//	public AlphaVector(Integer horizonNum, 
+//			Map.Entry<String, HashMap<String, ArithExpr>> actionEntry) {
+//		
+//		this.setHorizon(horizonNum);
+//		this.setAction(actionEntry);
+//				
+//		// Set the default value to be Zero
+////		this.setValueXADD(XADDHelper.ZeroXADD());
+//	}	
+	
+	public AlphaVector(Integer horizonNum, String actionName) {
+		
+		this.setHorizon(horizonNum);
+		this.setActionName(actionName);
+				
+		// Set the default value to be Zero
+//		this.setValueXADD(XADDHelper.ZeroXADD());
+	}		
+	
 	public AlphaVector(Integer horizonNum) {
 		
 		this.setHorizon(horizonNum);
-		this.setAction(null);
-		this.setObservation(null);		
-		this.setValueXADD(XADDHelper.ZeroXADD());
+//		this.setAction(null);
+//		this.setObservation(null);		
+//		this.setValueXADD(XADDHelper.ZeroXADD());
 	}
 	
 	/**
@@ -76,12 +110,7 @@ public class AlphaVector {
 	 */
 	public String getActionName() {
 		
-		if(this.action == null) {
-			return "";
-		}
-		else {
-			return this.action.getKey();
-		}
+		return this.actionName;		
 	}
 
 	/**
@@ -114,5 +143,29 @@ public class AlphaVector {
 										alphaVector.getValueXADD(), XADD.SUM);		
 		this.setValueXADD(newAlphaVectorXADD);
 	}
-	
+
+	@Override
+	public String toString() {		
+		
+		String strFormat = String.format("AVec (H:%d", this.getHorizon());
+		
+		if(this.getActionName() != null) {
+			strFormat = String.format("%s A:%s", strFormat, this.getActionName());
+		}
+		
+		if(this.getObservationName() != null) {
+			strFormat = String.format("%s O:%s", strFormat, this.getObservationName());
+		}
+
+		strFormat = String.format("%s)", strFormat);
+		
+		return strFormat;		
+		
+//		String str = String.format("AVec H:%d A:%s O:%s", 
+//				this.getHorizon(),  
+//				this.getActionName(), 
+//				this.getObservationName());
+//		
+//		return str;
+	}
 }
