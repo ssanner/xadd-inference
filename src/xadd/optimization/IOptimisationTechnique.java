1001dc1294707b7d7038e80f02171089fb664f49
S K <skin.dev.null@gmail.com>
2016-08-04 21:37:29 +1000

    Add interface for Optimisation methods

--- src/xadd/optimization/IOptimisationTechnique.java
+++ src/xadd/optimization/IOptimisationTechnique.java
@@ -11,10 +11,11 @@ public interface IOptimisationTechnique {
     /**
      *
      * @param objective
+     * @param variables
      * @param constraints
      * @param lowerBounds
      * @param upperBounds
      */
-    public double run(String objective, Collection<String> constraints,
+    public double run(String objective, Set<String> variables, Collection<String> constraints,
                          Collection<String> lowerBounds, Collection<String> upperBounds);
 }
