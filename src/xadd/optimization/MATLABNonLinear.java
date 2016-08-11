5feac6dddc9a69071f4fddac5551a6b0c56add87
S K <skin.dev.null@gmail.com>
2016-08-11 21:13:57 +1000

    Run fmincon with templated objective and constraints files

--- src/xadd/optimization/MATLABNonLinear.java
+++ src/xadd/optimization/MATLABNonLinear.java
@@ -23,6 +23,7 @@ public class MATLABNonLinear implements IOptimisationTechnique {
 
         this.cfg = new Configuration(Configuration.VERSION_2_3_25);
 
+        // Load the template directory
         try {
             this.cfg.setDirectoryForTemplateLoading(new
                     File("/Users/skin/repository/xadd-inference-1/src/xadd/optimization/templates"));
@@ -36,7 +37,7 @@ public class MATLABNonLinear implements IOptimisationTechnique {
             System.exit(1);
         }
 
-        //
+        // Create a MatlabProxyFactory instance
         if(MATLABNonLinear.factory == null) {
             MatlabProxyFactoryOptions options = new MatlabProxyFactoryOptions.Builder()
                     .setHidden(true)
@@ -45,6 +46,7 @@ public class MATLABNonLinear implements IOptimisationTechnique {
             MATLABNonLinear.factory = new MatlabProxyFactory(options);
         }
 
+        // Get a MatlabProxy
         try {
             this.proxy = MATLABNonLinear.factory.getProxy();
         } catch (MatlabConnectionException e) {
@@ -67,31 +69,40 @@ public class MATLABNonLinear implements IOptimisationTechnique {
                       Collection<String> upperBounds) {
 
         double valueObjectiveFunction = 0.0;
-        double solutionX = 0.0;
+        double solutionX;
 
         // convert the variables Set into a HashMap
-        HashMap<Integer, String> variablesMap = new HashMap<Integer, String>();
+        ArrayList<HashMap<String, Object>> variableList = new ArrayList<HashMap<String, Object>>();
 
         Integer varCount = 1;
         for(String var : variables) {
-            variablesMap.put(varCount++, var);
+            HashMap<String, Object> tmpMap = new HashMap<String, Object>();
+            tmpMap.put("symbol", var);
+            tmpMap.put("index", varCount++);
+
+            variableList.add(tmpMap);
         }
 
-        this.writeObjectiveFile(objective, variablesMap);
-        this.writeConstraintsFile(constraints, lowerBounds, upperBounds);
+        // Create the objective and constraints *.m files
+        this.createObjectiveFile(objective, variableList);
+        this.createConstraintsFile(constraints, lowerBounds, upperBounds, variableList);
 
+        // Run MATLAB's fmincon function with the objective and constraint files
         try {
-//            proxy = factory.getProxy();
 
-            this.proxy.eval("res = fmincon(@sir_path_2_h3, [0 0], [], [], [], [], [0 0], [100 0.1], @sir_path_2_h3_constraints)");
+            this.proxy.eval("cd('/Users/skin/repository/xadd-inference-1/src/cmomdp/')");
+//            this.proxy.eval("res = fmincon(@sir_path_2_h3, [0 0], [], [], [], [], [0 0], [100 0.1], @sir_path_2_h3_constraints)");
+            this.proxy.eval("res = fmincon(@obj_test, [0 0 0 0], [], [], [], [], [-10000 -10000 -10000 -10000], [10000 10000 10000 10000], @constraints_test)");
 
-            //returns the value of the objective function FUN at the solution X.
+            // Returns the value of the objective function FUN at the solution X.
             valueObjectiveFunction = ((double[]) proxy.getVariable("res"))[0];
             solutionX = ((double[]) proxy.getVariable("res"))[1];
+
             System.out.println("Result: " + valueObjectiveFunction + " at x = " + solutionX);
 
-            this.proxy.disconnect();
             this.proxy.exit();
+            this.proxy.disconnect();
+
         } catch (MatlabInvocationException e) {
             e.printStackTrace();
         }
@@ -100,50 +111,56 @@ public class MATLABNonLinear implements IOptimisationTechnique {
     }
 
     /**
+     *  Write data to a templated file
      *
-     * @param objective
-     * @param variablesMap
-     * @return
+     * @param data
+     * @param templateFileName
+     * @param outFileName
      */
-    private Boolean writeObjectiveFile(String objective, HashMap<Integer, String> variablesMap) {
+    private void writeDataToTemplateFile(HashMap<String, Object> data, String templateFileName, String outFileName) {
 
-        System.out.println("Minimise: " + objective);
+        Writer outFile;
+        Template objectiveTemp;
 
-        HashMap<String, Object> root = new HashMap<String, Object>();
-        root.put("objective", objective);
-
-        ArrayList<HashMap<String, Object>> variableList = new ArrayList<HashMap<String, Object>>();
-
-        for(Map.Entry<Integer, String> entry : variablesMap.entrySet()) {
-            HashMap<String, Object> tmpMap = new HashMap<String, Object>();
-            tmpMap.put("symbol", entry.getValue());
-            tmpMap.put("index", entry.getKey());
-
-            variableList.add(tmpMap);
-        }
-
-        root.put("variables", variableList);
-
-//        ArrayList<HashMap<String, Integer>> constantList = new ArrayList<HashMap<String, Integer>>();
-//        root.put("constants", constantList);
-
-        Template temp;
         try {
-            temp = this.cfg.getTemplate("MATLAB_objective.ftlh");
 
-            Writer out = new OutputStreamWriter(System.out);
-            temp.process(root, out);
+            objectiveTemp = this.cfg.getTemplate(templateFileName);
+            outFile = new FileWriter (new File("/Users/skin/repository/xadd-inference-1/src/cmomdp/" + outFileName));
 
-            out.close();
+            objectiveTemp.process(data, outFile);
+
+            outFile.flush();
+            outFile.close();
 
         } catch (IOException e) {
             e.printStackTrace();
             System.exit(1);
         } catch (TemplateException e) {
             e.printStackTrace();
+            System.err.println("Unable to write data to " + templateFileName);
+            System.exit(1);
         }
 
-        return Boolean.TRUE;
+    }
+
+    /**
+     *
+     * @param objective
+     * @param variableList
+     * @return  File name of the objective file
+     */
+    private void createObjectiveFile(String objective, ArrayList<HashMap<String, Object>> variableList) {
+
+        HashMap<String, Object> data = new HashMap<String, Object>();
+        data.put("objective", objective);
+        data.put("variables", variableList);
+
+
+
+//        ArrayList<HashMap<String, Integer>> constantList = new ArrayList<HashMap<String, Integer>>();
+//        data.put("constants", constantList);
+
+        this.writeDataToTemplateFile(data, "MATLAB_objective.ftlh", "obj_test.m");
     }
 
     /**
@@ -151,26 +168,46 @@ public class MATLABNonLinear implements IOptimisationTechnique {
      * @param constraints
      * @param lowerBounds
      * @param upperBounds
-     * @return
+     * @param variableList
+     * @return  File name of the constraints file
      */
-    private Boolean writeConstraintsFile(Collection<String> constraints, Collection<String> lowerBounds,
-                                      Collection<String> upperBounds) {
+    private void createConstraintsFile(Collection<String> constraints, Collection<String> lowerBounds,
+                                         Collection<String> upperBounds, ArrayList<HashMap<String, Object>> variableList) {
 
         // Nonlinear equality constraints have to be in the form c(x) <= 0
         // Nonlinear equality constraints are of the form ceq(x) = 0.
 
-        System.out.println("Subject to:");
+        HashMap<String, Object> data = new HashMap<String, Object>();
+        data.put("variables", variableList);
+
+        // Constraints
+        ArrayList<HashMap<String, Object>> constraintList = new ArrayList<HashMap<String, Object>>();
+        Integer constraintCounter = 1;
 
         for(String constraint : constraints) {
-            System.out.println("\t" + constraint);
+            HashMap<String, Object> tmpMap = new HashMap<String, Object>();
+
+            tmpMap.put("index", constraintCounter);
+            tmpMap.put("function", "-1 * (" + constraint + ")");
+
+            constraintList.add(tmpMap);
+            constraintCounter++;
         }
 
-        lowerBounds.addAll(upperBounds);
+//        lowerBounds.addAll(upperBounds);
+//
+//        for(String bound : lowerBounds) {
+//            HashMap<String, Object> tmpMap = new HashMap<String, Object>();
+//
+//            tmpMap.put("index", constraintCounter);
+//            tmpMap.put("function", "-1 * (" + bound + ")");
+//
+//            constraintList.add(tmpMap);
+//            constraintCounter++;
+//        }
 
-        for(String bound : lowerBounds) {
-            System.out.println("\t" + bound);
-        }
+        data.put("constraints", constraintList);
 
-        return Boolean.TRUE;
+        this.writeDataToTemplateFile(data, "MATLAB_constraints.ftlh", "constraints_test.m");
     }
 }
